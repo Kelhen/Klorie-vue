f5963aaec87515f6e72e41dfd27e4b924897003d
Kelhen <Kelhen@users.noreply.github.com>
2019-03-14 16:55:33 +0900

    add scss and store module

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,14 +3,8 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+export const store = new Vuex.Store({
+  // ...
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {},
 });
