715bdc6b4c0bcae264b5372fcd4cb015008da7b2
Kelhen <Kelhen@users.noreply.github.com>
2019-03-28 17:29:40 +0900

    clean gql exemple

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="fetchData">Fetch data</el-button>
+    <p v-for="user in users" :key="user.id">{{ user.name }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Button } from 'element-ui';
 import HelloWorld from './components/HelloWorld.vue';
+// gql
+import USERS from '@/graphql/queries/Users.gql';
 
 @Component({
   components: {
-    HelloWorld,
+    'el-button': Button,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private users: any[] = [];
+
+  private fetchData() {
+    this.$apollo.query({ query: USERS }).then((response: any) => {
+      this.users = response.data.users;
+    });
+  }
+}
 </script>
 
 <style>
