855f36a3b0d1d4762ef696885b9f78d73af05729
Kelhen <Kelhen@users.noreply.github.com>
2019-04-11 17:56:38 +0900

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { User } from '@/store/modules/user';
-import { Food } from '@/store/modules/food';
-
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({
-  // modules: {
-  //   user : User,
-  //   food: Food,
-  // },
-});
-
+export const store = new Vuex.Store({});
